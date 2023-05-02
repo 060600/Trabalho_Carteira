c4855e008aa7397c9f30c8e4775f085007225e21
Leonardo <l.passos@ig.ind.br>
2023-05-02 09:28:42 -0300

    Trabalho carteira

---
+++ Contas/LibClasses/Carteira.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas.LibClasses
+{
+    public class Carteira
+    {
+        public double Saldo
+        {
+            get;
+            private set;
+        }
+  
+        public string Dono { get; set; }
+        public string CPF { get; set; }
+        public int numConta { get; set; }
+        public double limiteConta { get; set; }
+        public DateTime UltimaCobranca { get; set; }
+
+        // Validação de horário comercial.
+        public bool Sacar(double Valor, DateTime dataSistema) 
+        {
+
+            if (!(dataSistema.Hour >= 10 && dataSistema.Hour < 18))
+                return false;
+
+
+            if (Valor > (this.Saldo + limiteConta))
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        } 
+
+        public bool Sacar(double Valor)
+        {
+            if (Valor > this.Saldo)
+                return false;
+
+            this.Saldo -= Valor;
+            //this.Saldo = Saldo - Valor;
+            return true;
+        }
+
+        public bool Depositar(double Valor)
+        {
+            {
+                this.Saldo += Valor;
+                return true;
+            }               
+
+        }
+
+        // Metodo para cobrar a Tarifa
+        public bool CobrarTarifa(double valorTarifa, DateTime dataAtual)
+        {
+            if (dataAtual.AddMonths(-1) < UltimaCobranca)
+                return false;
+
+            this.Saldo -= valorTarifa;
+
+            // Atualizando para fazer o pagamento da próxima tarifa.
+            UltimaCobranca = dataAtual;
+
+            return true;
+        }
+
+
+
+        public bool Transferir
+            (Carteira destino, double valor)
+        {  
+            //se nao tiver saldo cancela transferencia retornando false
+            if (this.Saldo <= valor)
+                return false;
+
+
+            //Executa transferencia tirando da conta origram e deposinto na conta destino
+            this.Sacar(valor);
+            bool tOK = destino.Depositar(valor);
+            if (tOK)// se transferencia ocorreu com sucesso retorna true
+                return true;
+            else// caso ocorrer erro faz o rollback voltando dinheiro para conta de origem
+            {
+                this.Depositar(valor);
+                return false;
+            }
+        }
+    }
+}
